4bfbd1adfa0928c3e60c69e4b86b4197d3f886f0
Andrei Hodanenok <xoda91@gmail.com>
2022-11-05 08:14:01 +0300

    Add never() static factory to Once

--- src/main/java/ahodanenok/reactivestreams/Once.java
+++ src/main/java/ahodanenok/reactivestreams/Once.java
@@ -10,4 +10,11 @@ public abstract class Once<T> implements Publisher<T> {
     public static <T> Once<T> value(T value) {
         return new OnceValue(value);
     }
+
+    /**
+     * Crete publisher that never emits any signal except onSubscribe.
+     */
+    public static <T> Once<T> never() {
+        return new OnceNever();
+    }
 }
