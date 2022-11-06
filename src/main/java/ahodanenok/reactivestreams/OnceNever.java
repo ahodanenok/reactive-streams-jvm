18ed8411a6fbed95e22b5a52b838360f05e85294
Andrei Hodanenok <xoda91@gmail.com>
2022-11-06 16:20:05 +0300

    Don't call cancel in OnceNever as it never terminates

--- src/main/java/ahodanenok/reactivestreams/OnceNever.java
+++ src/main/java/ahodanenok/reactivestreams/OnceNever.java
@@ -6,10 +6,7 @@ public class OnceNever<T> extends Once<T> {
 
     @Override
     public void subscribe(Subscriber<? super T> subscriber) {
-        OnceNeverSubscription<T> subscription = new OnceNeverSubscription<>(subscriber);
-        subscriber.onSubscribe(subscription);
-        // todo: hmm, i shouldn't do it this way
-        subscription.cancel(); // todo: finally?
+        subscriber.onSubscribe(new OnceNeverSubscription<>(subscriber));
     }
 
     private static class OnceNeverSubscription<T> extends OnceSubscription<T> {
