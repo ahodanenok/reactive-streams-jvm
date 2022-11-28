8e0ed5bb3cd595279bd0cd40d4799b4cfd0b5695
Andrei Hodanenok <xoda91@gmail.com>
2022-11-28 22:50:17 +0300

    Add TicksPublisher

---
+++ src/test/java/ahodanenok/reactivestreams/publisher/TicksPublisherTest.java
@@ -0,0 +1,111 @@
+package ahodanenok.reactivestreams.publisher;
+
+import java.util.concurrent.TimeUnit;
+
+import org.reactivestreams.tck.TestEnvironment;
+import org.reactivestreams.tck.TestEnvironment.ManualSubscriberWithSubscriptionSupport;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TicksPublisherTest {
+
+    @Test
+    public void shouldGenerateOneTick() throws Exception {
+        ManualSubscriberWithSubscriptionSupport<Long> subscriber =
+            new ManualSubscriberWithSubscriptionSupport<>(new TestEnvironment());
+        TicksPublisher publisher = new TicksPublisher(1, 5, TimeUnit.MILLISECONDS);
+        publisher.subscribe(subscriber);
+        subscriber.expectNext(0L, 20L);
+        subscriber.expectCompletion(100L);
+        subscriber.expectNone(100L);
+    }
+
+    @Test
+    public void shouldGenerateTwoTicks() throws Exception {
+        ManualSubscriberWithSubscriptionSupport<Long> subscriber =
+            new ManualSubscriberWithSubscriptionSupport<>(new TestEnvironment());
+        TicksPublisher publisher = new TicksPublisher(2, 2, TimeUnit.MILLISECONDS);
+        publisher.subscribe(subscriber);
+        subscriber.expectNext(0L, 20L);
+        subscriber.expectNext(1L, 20L);
+        subscriber.expectCompletion(100L);
+        subscriber.expectNone(100L);
+    }
+
+    @Test
+    public void shouldGenerateTenTicks() throws Exception {
+        ManualSubscriberWithSubscriptionSupport<Long> subscriber =
+            new ManualSubscriberWithSubscriptionSupport<>(new TestEnvironment());
+        TicksPublisher publisher = new TicksPublisher(10, 3, TimeUnit.MILLISECONDS);
+        publisher.subscribe(subscriber);
+        subscriber.expectNext(0L, 20L);
+        subscriber.expectNext(1L, 20L);
+        subscriber.expectNext(2L, 20L);
+        subscriber.expectNext(3L, 20L);
+        subscriber.expectNext(4L, 20L);
+        subscriber.expectNext(5L, 20L);
+        subscriber.expectNext(6L, 20L);
+        subscriber.expectNext(7L, 20L);
+        subscriber.expectNext(8L, 20L);
+        subscriber.expectNext(9L, 20L);
+        subscriber.expectCompletion(100L);
+        subscriber.expectNone(100L);
+    }
+
+    @Test
+    public void shouldGenerateTicksAfterPeriod() throws Exception {
+        ManualSubscriberWithSubscriptionSupport<Long> subscriber =
+            new ManualSubscriberWithSubscriptionSupport<>(new TestEnvironment());
+        TicksPublisher publisher = new TicksPublisher(100, 5, TimeUnit.MILLISECONDS);
+        publisher.subscribe(subscriber);
+        Thread.sleep(26L);
+        subscriber.cancel();
+        subscriber.expectNext(0L, 1L);
+        subscriber.expectNext(1L, 1L);
+        subscriber.expectNext(2L, 1L);
+        subscriber.expectNext(3L, 1L);
+        subscriber.expectNext(4L, 1L);
+        subscriber.expectNone(100L);
+    }
+
+    @Test
+    public void shouldThrowNpeIfTimeUnitNull() {
+        assertThrows(NullPointerException.class, () -> new TicksPublisher(1, 1, null));
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentIfCountInvalid() {
+        assertThrows(IllegalArgumentException.class, () -> new TicksPublisher(-10, 1, TimeUnit.SECONDS));
+        assertThrows(IllegalArgumentException.class, () -> new TicksPublisher(-1, 1, TimeUnit.SECONDS));
+        assertThrows(IllegalArgumentException.class, () -> new TicksPublisher(0, 1, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentIfPeriodInvalid() {
+        assertThrows(IllegalArgumentException.class, () -> new TicksPublisher(10, -10, TimeUnit.SECONDS));
+        assertThrows(IllegalArgumentException.class, () -> new TicksPublisher(1, -1, TimeUnit.SECONDS));
+        assertThrows(IllegalArgumentException.class, () -> new TicksPublisher(2, 0, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void shouldThrowNpeIfSubscriberNull() {
+        TicksPublisher publisher = new TicksPublisher(1, 1, TimeUnit.SECONDS);
+        assertThrows(NullPointerException.class, () -> publisher.subscribe(null));
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = { 0, -1, -10, Long.MIN_VALUE })
+    public void shouldThrowIllegalArgumentIfRequestedAmountNotValid(long count) throws Exception {
+        ManualSubscriberWithSubscriptionSupport<Long> subscriber =
+            new ManualSubscriberWithSubscriptionSupport<>(new TestEnvironment());
+        TicksPublisher publisher = new TicksPublisher(10, 5, TimeUnit.MILLISECONDS);
+        publisher.subscribe(subscriber);
+        subscriber.request(count);
+        subscriber.expectError(IllegalArgumentException.class);
+        subscriber.expectNone();
+    }
+}
