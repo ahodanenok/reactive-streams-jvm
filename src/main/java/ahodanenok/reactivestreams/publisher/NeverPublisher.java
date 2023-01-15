bce70edd044426e1e0cf30455c28f90e8002bc20
Andrei Hodanenok <xoda91@gmail.com>
2023-01-15 15:24:12 +0300

    Implement NeverPublisher with AbstractPublisherV2

--- src/main/java/ahodanenok/reactivestreams/publisher/NeverPublisher.java
+++ src/main/java/ahodanenok/reactivestreams/publisher/NeverPublisher.java
@@ -1,18 +1,6 @@
 package ahodanenok.reactivestreams.publisher;
 
-import org.reactivestreams.Subscriber;
+public class NeverPublisher<T> extends AbstractPublisherV2<T> {
 
-public class NeverPublisher<T> extends AbstractPublisher<T> {
-
-    @Override
-    protected void doSubscribe(Subscriber<? super T> subscriber) {
-        subscriber.onSubscribe(new NeverPublisherSubscription<>(subscriber));
-    }
-
-    static class NeverPublisherSubscription<T> extends AbstractSubscription<T> {
-
-        NeverPublisherSubscription(Subscriber<? super T> subscriber) {
-            super(subscriber);
-        }
-    }
+    // no-op
 }
